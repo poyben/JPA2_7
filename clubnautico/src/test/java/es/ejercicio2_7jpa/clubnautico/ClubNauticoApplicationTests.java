6d4a3ea55357b9d6dff29fd316dcde907e4d356d
poyben <poybenglez@gmail.com>
2024-04-04 11:49:11 +0100

    Creado el proyecto

---
+++ clubnautico/src/test/java/es/ejercicio2_7jpa/clubnautico/ClubNauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ejercicio2_7jpa.clubnautico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
