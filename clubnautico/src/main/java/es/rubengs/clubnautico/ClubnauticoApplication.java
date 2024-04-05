902074408924deef2d44d0fdf4b6539c20d6982c
poyben <poybenglez@gmail.com>
2024-04-05 11:11:44 +0100

    Recreado el proyecto

---
+++ clubnautico/src/main/java/es/rubengs/clubnautico/ClubnauticoApplication.java
@@ -0,0 +1,13 @@
+package es.rubengs.clubnautico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubnauticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubnauticoApplication.class, args);
+	}
+
+}
---
+++ clubnautico/src/test/java/es/rubengs/clubnautico/ClubnauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.rubengs.clubnautico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubnauticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
